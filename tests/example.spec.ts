d01d4532f3922720e4aef6c9061109a0c1ced453
tkkkd <>
2024-09-25 00:50:37 +0900

    vrt test memorable first try

---
+++ tests/example.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.click(
+    "text=When to Use Static Generation v.s. Server-side Rendering",
+  );
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveURL("http://localhost:3000/posts/ssg-ssr");
+});
