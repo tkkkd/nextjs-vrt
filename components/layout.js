790274f29ff88b8222553bedafa71f5900cc0ed5
tkkkd <98257555+tkkkd@users.noreply.github.com>
2024-10-03 22:23:14 +0900

    thumbs up title (#6)
    Co-authored-by: tkkkd <>

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "☀️";
+const name = "👍😄❤️🐉";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
